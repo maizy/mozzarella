70a886d7925f489cb6ad84c0ce3ca36c1066134c
Nikita Kovaliov <nikita@maizy.ru>
2019-04-27 23:57:14 +0300

    iss #5: [wip]

--- minetest-test-app/src/main/scala/space/maizy/mozzarella/minetest_test_app/TestAppLauncher.scala
+++ minetest-test-app/src/main/scala/space/maizy/mozzarella/minetest_test_app/TestAppLauncher.scala
@@ -1,6 +1,16 @@
 package space.maizy.mozzarella.minetest_test_app
 
+/**
+ * Copyright (c) Nikita Kovaliov, maizy.space, 2019
+ * See LICENSE.txt for details.
+ */
+
 import java.net.InetSocketAddress
+import scala.util.Random
+import scodec.bits.BitVector
+import space.maizy.mozzarella.minetest_proto.control.{ ControlAck, ControlPacket, ControlSetPeerId }
+import space.maizy.mozzarella.minetest_proto.data.ControlType
+import space.maizy.mozzarella.minetest_proto.original.{ ToClientHello, ToClientOriginalPacket, ToServerInit }
 // import scala.concurrent.ExecutionContext
 import akka.actor.{ Actor, ActorLogging, ActorRef, ActorSystem, Props }
 import akka.io.{ IO, UdpConnected }
@@ -10,26 +20,25 @@ import space.maizy.mozzarella.minetest_proto.{ PacketOnWire, Protocol }
 import space.maizy.mozzarella.minetest_proto.data.PacketType
 import space.maizy.mozzarella.minetest_proto.original.ToServerEmpty
 import space.maizy.mozzarella.minetest_proto.reliable.ReliablePacket
-import space.maizy.mozzarella.minetest_proto.utils.Printer
-
-/**
- * Copyright (c) Nikita Kovaliov, maizy.space, 2019
- * See LICENSE.txt for details.
- */
 
 class TestAuthActor(remote: InetSocketAddress) extends Actor with ActorLogging {
   import context.system
 
   var packetSeq = 65500
+  var peerId: Int = 0
+
   IO(UdpConnected) ! UdpConnected.Connect(self, remote)
 
-  /**
+  /*
+
+for any reliable packet:
+-> ch:0, peer:3, Control(Ack: #65500)
+
 #0 -> ch:0, peer:unassigned, ReliablePacket(#65500: ToServerOriginalPacket(EMPTY))
 #1 <- ch:0, peer:server, ReliablePacket(#65500: Control(SetPeerId: newPeerId: 3))
-#2 <- ch:0, peer:server, Control(Ack: #65500)
-#3 -> ch:0, peer:3     , Control(Ack: #65500)
 #4 -> ch:1, peer:3     , ToServerInit(serializationVersion: 28, minProto: 37, maxProto: 37, player: maizy)
 #5 <- ch:0, peer:server, ReliablePacket(#65501: ToClientHello(serializationVersion: 28,compressionMode: 0, proto: 37, allowedAuthMechanism: 2, legacyPlayerNameCasing: maizy))
+
    */
 
   private def nextSeq: Int = {
@@ -43,15 +52,90 @@ class TestAuthActor(remote: InetSocketAddress) extends Actor with ActorLogging {
 
   def receive: Receive = {
     case UdpConnected.Connected =>
+      log.info("connected")
       context.become(ready(sender()))
       val initConnectionPacket = ReliablePacket(nextSeq, PacketType.Original, ToServerEmpty)
-      self ! PacketOnWire(channel = 0, peerId = 0, initConnectionPacket)
+      self ! PacketOnWire(channel = 0, peerId = peerId, initConnectionPacket)
   }
 
   def ready(connection: ActorRef): Receive = {
     case UdpConnected.Received(data) =>
       // process data, send it on, etc.
-      log.info("receive data: {}", data)
+      Protocol.decodeToClient(BitVector(data.toByteBuffer)) match {
+        case Left(errors) =>
+          log.error("unparsable response {}: {}", data, errors)
+        case Right(packet) =>
+          log.info("receive: {}", packet)
+
+          // first: assign new pid
+          packet.packet match {
+            case ReliablePacket(
+              seqNum,
+              PacketType.Control,
+              ControlPacket(
+                ControlType.SetPeerId,
+                ControlSetPeerId(newPeerId)
+              )
+            ) =>
+              log.info("peer id assigned: {}", newPeerId)
+              peerId = newPeerId
+            case _ =>
+          }
+
+          // response with ack to any reliable packet
+          packet.packet match {
+            case ReliablePacket(seqNum, _, _) =>
+              val ack = PacketOnWire(
+                channel = 0,
+                peerId = peerId,
+                ControlPacket(ControlType.Ack, ControlAck(seqNum))
+              )
+
+              self ! ack
+            case _ =>
+          }
+
+          // process messages
+          packet.packet match {
+            case ReliablePacket(
+              seqNum,
+              PacketType.Control,
+              ControlPacket(
+                ControlType.SetPeerId,
+                _
+              )
+            ) =>
+              val toServerInit = PacketOnWire(
+                channel = 1,
+                peerId = peerId,
+                ToServerInit(
+                  serializationVersion = 28,
+                  compressionMode = 0,
+                  minProtoVersion = 37,
+                  maxProtoVersion = 37,
+                  playerName = s"test-${Random.nextInt(100000)}"
+                )
+              )
+
+              self ! toServerInit
+
+            case ReliablePacket(
+              seqNum,
+              PacketType.Original,
+              originalPacket: ToClientOriginalPacket
+            ) =>
+              originalPacket match {
+                case h: ToClientHello =>
+                  if (h.serializationVersion != 28 || h.compressionMode != 0 || h.protoVersion != 37) {
+                    log.error("unsupported protocol version: {}", h)
+                    self ! UdpConnected.Disconnect
+                  }
+                case _ =>
+              }
+
+            case _ => log.error("unhandled")
+          }
+      }
 
 
     case p: PacketOnWire =>
@@ -59,13 +143,13 @@ class TestAuthActor(remote: InetSocketAddress) extends Actor with ActorLogging {
         case Left(errors) =>
           log.error("Unable to encode packet to server: {}", errors)
         case Right(bits) =>
-          log.info("send packet: {}", p)
-          log.info("packet bytes: {}", Printer.byteVectorToString(bits.toByteVector))
+          log.info("send   : {}", p)
           val bs = ByteString(bits.toByteArray)
-          connection ! bs
+          connection ! UdpConnected.Send(bs)
       }
 
     case UdpConnected.Disconnect => connection ! UdpConnected.Disconnect
+
     case UdpConnected.Disconnected => context.stop(self)
   }
 }
